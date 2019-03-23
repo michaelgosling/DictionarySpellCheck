f43fa2ec13561e3c105fea837e82217a8c137b0a
Michael Gosling <contact@michaelgosling.me>
2019-03-23 16:13:50 -0300

    Initial Commit

---
+++ DictionarySpellCheck/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DictionarySpellCheck
+//
+//  Created by Michael Gosling on 2019-03-23.
+//  Copyright © 2019 Michael Gosling. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
